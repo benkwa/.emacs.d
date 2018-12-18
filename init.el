a53617c7dc8c13d7553611b0c02c6d677618623d
Ben Kwa <ben.kwa@dreamworks.com>
2018-12-17 22:30:51 -0800

    Add dropbox support.

--- init.el
+++ init.el
@@ -17,6 +17,10 @@
 (add-to-list 'package-archives
              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
+(require 'dropbox)
+(if (require 'bk-dropbox-token nil t)
+    (setq dropbox-access-token bk-dropbox-access-token))
+
 (with-eval-after-load "git-commit-mode" (debug))
 (defun git-commit-mode (&rest args) nil)
 
@@ -530,6 +534,68 @@ that uses 'compilation-error-face'."
 (add-hook 'c++-mode-hook 'subword-mode)
 (add-hook 'c++-mode-hook 'electric-indent-mode)
 (add-hook 'c++-mode-hook 'electric-pair-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; org stuff
+
+(require 'org)
+(require 'org-protocol)
+;; Store links in any buffers
+(define-key global-map "\C-cl" 'org-store-link)
+;; Call up agenda from any buffer
+(define-key global-map "\C-ca" 'org-agenda)
+;; Log timestamps for done items
+(setq org-log-done t)
+;; Start in indented mode
+(setq org-startup-indented t)
+(setq org-tags-column -80)
+
+(setq org-agenda-files (list "~/org/_inbox.org" 
+                             "~/org/home.org"
+                             "~/org/ping.org"
+                             ))
+
+(setq org-refile-targets
+      '((nil :maxlevel . 1)
+        (org-agenda-files :maxlevel . 1)
+        ("~/org/done.org" :maxlevel . 1)
+        ("~/org/later.org" :maxlevel . 1)))
+
+(setq org-capture-templates
+      (quote
+       (("w"
+         "Default template"
+         entry
+         (file+headline "~/org/inbox.org" "Inbox")
+         "* %^{Title}\n\n  Source: %u, %c\n\n  %i"
+         :empty-lines 1)
+        ("l"
+         "Link template"
+         entry
+         (file+headline "~/org/inbox.org" "Inbox")
+         "* %c %T"
+         :empty-lines 1
+         :immediate-finish 1)
+        ;; ... more templates here ...
+        )))
+(defadvice org-capture
+    (after make-full-window-frame activate)
+  "Advise capture to be the only window when used as a popup"
+  (if (equal "emacs-capture" (frame-parameter nil 'name))
+      (delete-other-windows)))
+
+(defadvice org-capture-finalize
+    (after delete-capture-frame activate)
+  "Advise capture-finalize to close the frame"
+  (if (equal "emacs-capture" (frame-parameter nil 'name))
+      (delete-frame)))
+
+(defun bk-org-agenda-mode-hook ()
+  (local-set-key (kbd "j") 'org-agenda-next-line)
+  (local-set-key (kbd "k") 'org-agenda-previous-line)
+  )
+
+(add-hook 'org-agenda-mode-hook 'bk-org-agenda-mode-hook)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
